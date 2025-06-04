fba153f7a4913641b9c9eadda5e03043ba6a86cb
Reza-Keymasi <rezakeymasi1996@gmail.com>
2025-06-04 11:11:33 +0330

    separate client and server logic

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,32 +2,9 @@
 
 import { ReactNode } from "react";
 
-import {
-  isServer,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-export function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
+import { getQueryClient } from "./get-query-client";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
