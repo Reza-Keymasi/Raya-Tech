fc30774ac75f779f3b33e49171412b21fd9872ac
Reza-Keymasi <rezakeymasi1996@gmail.com>
2025-06-05 12:49:46 +0330

    add seach option for user table

--- src/app/users/UsersTable.tsx
+++ src/app/users/UsersTable.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { ChangeEvent, useState } from "react";
+
 import Table from "@/components/table/Table";
 import { IUser } from "@/types/IUser";
 import { useGetUsers } from "@/lib/react-query/userQueries";
+import Input from "@/components/input/Input";
 
 const columns: { header: string; accessor: keyof UserTable }[] = [
   { header: "#", accessor: "id" },
@@ -18,21 +21,36 @@ type UserTable = Pick<
 >;
 
 export default function UsersTable() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   const { data: users } = useGetUsers();
 
-  const userData: UserTable[] =
-    users?.map(({ id, name, username, email, phone, website }) => ({
-      id,
-      name,
-      username,
-      email,
-      phone,
-      website,
-    })) || [];
+  const filteredData = users?.filter((user) =>
+    Object.entries(user).some((value) =>
+      String(value).toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  );
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setSearchQuery(e.target.value);
+  }
 
   return (
     <div className="mx-auto mt-10 px-4 w-11/12">
-      <Table data={userData || []} columns={columns} />
+      <div className="py-3 w-1/4">
+        <Input
+          name="Search Info"
+          type="text"
+          onChange={handleChange}
+          value={searchQuery}
+        />
+      </div>
+      <Table
+        data={filteredData || []}
+        columns={columns}
+        onDetails={() => {}}
+        onDelete={() => {}}
+      />
     </div>
   );
 }
