d2d160cfc12e42e7475b5188a4432f820c06d9af
Reza-Keymasi <rezakeymasi1996@gmail.com>
2025-06-06 13:34:17 +0330

    create user data types

---
+++ src/types/IUser.ts
@@ -0,0 +1,29 @@
+export interface IGeoLocation {
+  lat: string;
+  lng: string;
+}
+
+export interface IAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: IGeoLocation;
+}
+
+export interface ICompany {
+  bs: string;
+  catchPhrase: string;
+  name: string;
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: IAddress;
+  company: ICompany;
+}
