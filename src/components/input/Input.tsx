3d6c45af4a595f613d212ed1dd20bc894b17332f
Reza-Keymasi <rezakeymasi1996@gmail.com>
2025-06-06 13:10:43 +0330

    develop input component for error handling

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,25 +1,48 @@
-import { ChangeEvent, ComponentPropsWithoutRef } from "react";
+import { ChangeEvent, ComponentPropsWithoutRef, FocusEvent } from "react";
 
 type InputProps = {
+  error?: string | null;
+  name?: string;
+  label: string;
+  onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 } & ComponentPropsWithoutRef<"input">;
 
-export default function Input({ onChange, ...props }: InputProps) {
+export default function Input({
+  error,
+  label,
+  name,
+  onBlur,
+  onChange,
+  ...props
+}: InputProps) {
+  const modifiedLabel = label?.charAt(0).toUpperCase() + label?.slice(1);
   return (
-    <div className="relative">
+    <div className="relative mb-2 w-full">
       <input
-        className="peer h-12 w-full bg-transparent text-white px-2 rounded-lg border border-gray-300/60 focus:border-indigo-200 focus:outline-0 appearance-none"
-        id={props.name}
+        className={`peer h-12 w-full bg-transparent text-white px-2 rounded-lg border ${
+          error
+            ? "border-orange-400"
+            : "border-gray-300/60 focus:border-indigo-200"
+        } focus:outline-0 appearance-none`}
+        id={label}
+        name={name}
+        onBlur={onBlur}
         onChange={onChange}
         placeholder=" "
         {...props}
       />
       <label
-        className="flex items-center absolute top-3 left-2 text-sm transition-all duration-150 ease-in peer-focus:top-0.5 peer-focus:text-indigo-200 peer-focus:text-xs peer-[:not(:placeholder-shown)]:top-0.5 peer-[:not(:placeholder-shown)]:text-xs"
-        htmlFor={props.name}
+        className={`flex items-center absolute top-3 left-2 text-sm transition-all duration-150 ease-in peer-focus:top-0.5  ${
+          error ? "text-orange-400" : "peer-focus:text-indigo-200"
+        } peer-focus:text-xs peer-[:not(:placeholder-shown)]:top-0.5 peer-[:not(:placeholder-shown)]:text-xs`}
+        htmlFor={label}
       >
-        {props.name}
+        {modifiedLabel}
       </label>
+      <div className="h-1 text-[10px] capitalize text-orange-400 pl-2 pt-0.5">
+        {error}
+      </div>
     </div>
   );
 }
